eb28aad8741db8d4a7f50e272e97166128362673
dotX12 <hash@shitposting.team>
2021-01-31 21:40:29 +0300

    ✨ Transfer token

--- examples/telebot/misc.py
+++ examples/telebot/misc.py
@@ -1,9 +1,6 @@
 import telebot
-
+from settings import *
 from API import BlockcypherAPI
-
-API_TOKEN = '1085642934:AAG9eNsHj2dsZlVup9bFhYLQJ7F_G8DuMjo'
-ADMIN_ID = '227020931'
 
 bot = telebot.TeleBot(API_TOKEN)
 event = BlockcypherAPI()
